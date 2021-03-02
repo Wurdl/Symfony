289401dda72a2cf4ce1cd19e469746e1955b5928
Jakob <jakob.ehmann@intouch.at>
2021-03-02 15:57:52 +0100

    added twig & templates

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -4,8 +4,9 @@ namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class QuestionController
+class QuestionController extends AbstractController
 {
     
     /**
@@ -20,10 +21,18 @@ class QuestionController
      * @Route("/questions/{slug}")
      */
 
-    public function show($slug){
-        return new Response(sprintf(
-            'Future page to show a question "%s"!',
-            ucwords(str_replace('-', ' ', $slug))
-            ));
+    public function show($slug)
+    {
+
+        $answers = [
+            'Make sure your cat is sitting purrrrrfectly still',
+            'Honestly, I like furry shoes better than MY cat',
+            'Maybe.... try saying the spell backwards?',
+        ];
+
+        return $this->render('question/show.html.twig', [
+            'question' => ucwords(str_replace('-', ' ', $slug)),
+            'answers' => $answers,
+        ]);
     }
 }
